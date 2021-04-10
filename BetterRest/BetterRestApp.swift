7b2cc712eaa2d9bfc7d4b02dc3fe1d3c6052ac30
Oluwasanmi <sanmiaderibigbe@outlook.com>
2021-04-10 08:28:07 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by sanmi_personal on 10/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
